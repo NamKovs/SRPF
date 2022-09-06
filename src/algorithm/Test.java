23bc0346d52e6c825eafa71975d0ba2c261451aa
NamKovs <48852438+NamKovs@users.noreply.github.com>
2022-09-06 16:41:50 +0800

    Update Test.java

--- src/algorithm/Test.java
+++ src/algorithm/Test.java
@@ -5,32 +5,29 @@ import java.io.FileReader;
 
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.TestInstances;
-import algorithm.SRDF;
+import algorithm.SRPF;
 
-public class Test {
-	
+public class Example {
 	public static void main(String[] args) throws Exception {
 
-		int numOfIterations = 100;  // The number of Iterations.
+		int numOfIterations = 20;  // The number of Iterations.
 		int sizeOfSamplePool = 100; // The size of sample pool.
-		int numOfKNeighbors = 3;   // The k-nearest neighbors.
-		int numOfExperiments = 20; // Number of experiments.
+		int numOfKNeighbors = 2;   // The k-nearest neighbors. 
+		int numOfExperiments = 10; // Number of experiments.
 		double ration = 0.05;	   // The percentage of labeled datasets in the training set.
-
 		for (int i = 0; i < numOfExperiments; i++) {
-			try {
-				BufferedReader s = new BufferedReader(new FileReader("src/algorithm/housing.arff"));
+			try {;
+				BufferedReader s = new BufferedReader(new FileReader("src/data/parkinsons.arff"));
 				Instances data = new Instances(s);
-				SRDF.normalize(data);
+//				SRDF.normalize(data);
 				data.setClassIndex(data.numAttributes() - 1);
 				s.close();
 
 				// Divide the dataset into training set and test set.
-				int[] tempRandArray = SRDF.getRandomOrder(data.numInstances());
+				int[] tempRandArray = SRDP_1.getRandomOrder(data.numInstances());
 				int tempTrainingSize = new Double(data.numInstances() * 0.7).intValue();
 
-				Instance tempInstances = null;
+				Instance tempInstances = null; 
 				Instances train = new Instances(data, 0);
 				Instances test = new Instances(data, 0);
 
@@ -45,21 +42,20 @@ public class Test {
 				
 				
 				// model training.
-				SRDF srpf = new SRDF(numOfIterations, sizeOfSamplePool, numOfKNeighbors, ration);
+				SRPF srpf = new SRPF(numOfIterations, sizeOfSamplePool, numOfKNeighbors, ration);
 				srpf.buildClassifier(train);
 				
 				// Prediction.
 				double rmse = 0;
-				
 				for (int n = 0; n < test.numInstances(); n++) {
 					Instance inst = test.instance(n);
 					double val = srpf.classifyInstance(inst);
 					rmse += (inst.classValue() - val) * (inst.classValue() - val);
 				}
-				System.out.println("rmse = " + Math.sqrt(rmse/ test.numInstances()));
+				System.out.println(Math.sqrt(rmse/ test.numInstances()));
 				} catch (Exception e) {
 				e.printStackTrace();
-			}//Of for i
-		}// Of i
+			}//Of for Try
+		}//Of for i
 	}//Of main
-}//Of class Test
+}
